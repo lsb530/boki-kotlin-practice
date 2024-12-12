4ac0c6f67bb7051787f8a0096063e20b0c7a05d9
boki <lsb530@naver.com>
2024-12-12 20:31:20 +0900

    arrange code style

--- src/main/java/basic/lec02/Person.java
+++ src/main/java/basic/lec02/Person.java
@@ -1,21 +1,20 @@
 package basic.lec02;
 
 import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
 
 public class Person {
 
-  private final String name;
+    private final String name;
 
-  public Person(String name) {
-    this.name = name;
-  }
+    public Person(String name) {
+        this.name = name;
+    }
 
-  // @Nullable
-  @NotNull
-  public String getName() {
-    return name;
-  }
+    // @Nullable
+    @NotNull
+    public String getName() {
+        return name;
+    }
 
 }
 
