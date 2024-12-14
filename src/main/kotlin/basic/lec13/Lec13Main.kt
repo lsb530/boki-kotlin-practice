e94245f2650b320f5f473615d6705cbc4a4c8926
boki <lsb530@naver.com>
2024-12-15 08:33:54 +0900

    중첩 클래스

---
+++ src/main/kotlin/basic/lec13/Lec13Main.kt
@@ -0,0 +1,26 @@
+package basic.lec13
+
+fun main() {
+    
+}
+
+class House(
+    private val address: String,
+    private val livingRoom: LivingRoom,
+) {
+    class LivingRoom(
+        private var area: Double,
+    )
+}
+
+class InnerHouse(
+    private val address: String,
+    private val livingRoom: LivingRoom,
+) {
+    inner class LivingRoom(
+        private var area: Double,
+    ) {
+        val address: String
+            get() = this@InnerHouse.address
+    }
+}
