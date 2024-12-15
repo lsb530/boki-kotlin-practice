46179f423451b8aada64d45e70aba1818f765cda
boki <lsb530@naver.com>
2024-12-15 10:30:13 +0900

    함수(extend, infix, inline, local)

---
+++ src/main/java/basic/lec16/Lec16Main.java
@@ -0,0 +1,12 @@
+package basic.lec16;
+
+import basic.d_fp.lec16.Lec16MainKt;
+
+public class Lec16Main {
+
+    public static void main(String[] args) {
+        char lastChar = Lec16MainKt.lastChar("ABCD");
+        System.out.println("lastChar = " + lastChar);
+    }
+
+}
