53e3060d288df5fa77844833a32e643be675ce4a
boki <lsb530@naver.com>
2024-12-17 22:21:00 +0900

    Reflection

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,6 +3,8 @@ plugins {
     // https://github.com/melix/jmh-gradle-plugin
     id("me.champeau.jmh") version "0.7.2"
     // id("me.champeau.gradle.jmh") version "0.5.3"
+    id("org.jetbrains.dokka") version "1.8.10"
+    id("org.jlleitschuh.gradle.ktlint") version "11.4.2"
 }
 
 group = "com.boki"
@@ -19,7 +21,12 @@ dependencies {
     implementation("io.github.oshai:kotlin-logging-jvm:7.0.0")
     // https://mvnrepository.com/artifact/org.slf4j/slf4j-simple
     implementation("org.slf4j:slf4j-simple:2.0.16")
+    // reflection
+    implementation("org.jetbrains.kotlin:kotlin-reflect:2.0.10")
+
+    implementation("org.reflections:reflections:0.10.2")
     testImplementation(kotlin("test"))
+    testImplementation("org.assertj:assertj-core:3.24.2")
 }
 
 tasks.test {
